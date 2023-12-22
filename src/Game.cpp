c8ad111db9b8c6f383796a6c11d6702bef14805e
Halal_Coding <gamerasmir134@gmail.com>
2023-12-22 12:05:38 +0500

    Update Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -6,18 +6,30 @@ using namespace std;
 
 class Game {
     private:
-        string spaces[9] = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
+        string spaces[9] = {"1", " ", " ", " ", " ", " ", " ", " ", " "};
         char player = 'X';
         char computer = 'O';
         bool running = true;
 
     public:
         void drawBoard(){
-
+            cout << "     |     |     " << endl;
+            cout << "     |     |     " << endl;
+            cout << "     |     |     " << endl;
+            cout << "-----------------" << endl;
+            cout << "     |     |     " << endl;
+            cout << "     |     |     " << endl;
+            cout << "     |     |     " << endl;
+            cout << "-----------------" << endl;
+            cout << "     |     |     " << endl;
+            cout << "     |     |     " << endl;
+            cout << "     |     |     " << endl;
         }
 };
 
 int main() {
     
+    Game g;
+    g.drawBoard();
     return 0;
 }
